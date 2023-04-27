4486ab0c29f3b4dd0bbe3873c5174c9b8ed99b9c
Turler <minhdaochi504@gmail.com>
2023-04-27 22:43:13 +0700

    change to username insteada author

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-    @movies = Movie.available
+    @movies = Movie.available.includes(:user)
   end
 
   def new
